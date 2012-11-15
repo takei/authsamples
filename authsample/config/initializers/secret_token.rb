a10ba613250064fbfb6d2c2f9a2e177d66cd3a1e
takei <hidetake0118@gmail.com>
2012-11-15 20:35:04 +0900

    First Commit

---
+++ authsample/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authsample::Application.config.secret_token = 'bc79a7009e3060cfbd483bda0389178b7272322f1588f8be8f272e6d30c62b1b942ada2e9cc12ab1b394dc97bb7610c85d56a757b45302600f9dbee86dd29477'
